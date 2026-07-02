f9f11583678b5117aa2dec88c033656402f91996
Antonio <trujillohernandezraquel@outlook.es>
2026-07-02 09:05:00 +0100

    Convertir todas las imágenes de productos Lenda a .jpg para la web

--- check_rls2.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient('', ''); // Service Role Key
-async function test() {
-  const { data, error } = await supabase.from('pg_policies').select('*').eq('tablename', 'clientes');
-  console.log('Policies:', JSON.stringify(data, null, 2));
-}
-test();
